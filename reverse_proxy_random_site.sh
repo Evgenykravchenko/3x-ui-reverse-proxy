bc3a4c01405cf334be6d290d26ce0508fe460af4
kaiscend <56931684+kaiscend@users.noreply.github.com>
2025-02-04 14:08:52 +0600

    Update reverse_proxy_random_site.sh

--- reverse_proxy_random_site.sh
+++ reverse_proxy_random_site.sh
@@ -31,10 +31,10 @@ apt-get update -y && apt-get install -y zip wget unzip
   msg_inf "Распаковываем landing-ideaplex.zip..."
   rm -rf landing-ideaplex
   unzip -q landing-ideaplex.zip -d landing-ideaplex || { msg_err "Ошибка при распаковке"; return 1; }
- cd landing-ideaplex
+
   msg_inf "Копируем содержимое в /var/www/html/..."
   rm -rf /var/www/html/*
-  cp -a landing-ideaplex/. /var/www/html/ || { msg_err "Ошибка при копировании"; return 1; }
+  cp -a landing-ideaplex/* /var/www/html/ || { msg_err "Ошибка при копировании"; return 1; }
 
   msg_ok "Лендинг успешно установлен!"
 cd ~
