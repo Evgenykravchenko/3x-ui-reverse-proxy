ee31534b4a68edd566d898f86faf5a25aa6420a6
Evgenykravchenko <33199373+Evgenykravchenko@users.noreply.github.com>
2025-02-04 15:00:52 +0600

    Update reverse_proxy_server.sh
    Change links

--- reverse_proxy_server.sh
+++ reverse_proxy_server.sh
@@ -186,8 +186,8 @@ E[69]="Enter the Telegram bot token for IP limit, Torrent ban:"
 R[69]="Введите токен Telegram бота для IP limit, Torrent ban:"
 E[70]="Secret key:"
 R[70]="Секретный ключ:"
-E[71]="Current operating system is \$SYS.\\\n The system lower than \$SYSTEM \${MAJOR[int]} is not supported. Feedback: [https://github.com/cortez24rus/xui-reverse-proxy/issues]"
-R[71]="Текущая операционная система: \$SYS.\\\n Система с версией ниже, чем \$SYSTEM \${MAJOR[int]}, не поддерживается. Обратная связь: [https://github.com/cortez24rus/xui-reverse-proxy/issues]"
+E[71]="Current operating system is \$SYS.\\\n The system lower than \$SYSTEM \${MAJOR[int]} is not supported. Feedback: [https://github.com/Evgenykravchenko/3x-ui-reverse-proxy/issues]"
+R[71]="Текущая операционная система: \$SYS.\\\n Система с версией ниже, чем \$SYSTEM \${MAJOR[int]}, не поддерживается. Обратная связь: [https://github.com/Evgenykravchenko/3x-ui-reverse-proxy/issues]"
 E[72]="Install dependence-list:"
 R[72]="Список зависимостей для установки:"
 E[73]="All dependencies already exist and do not need to be installed additionally."
@@ -1037,7 +1037,7 @@ dns_adguard_home() {
   HASH=$(htpasswd -B -C 10 -n -b ${USERNAME} ${PASSWORD} | cut -d ":" -f 2)
 
   rm -f AdGuardHome/AdGuardHome.yaml
-  while ! wget -q --progress=dot:mega --timeout=30 --tries=10 --retry-connrefused "https://github.com/cortez24rus/xui-reverse-proxy/raw/refs/heads/main/adh/AdGuardHome.yaml" -O AdGuardHome/AdGuardHome.yaml; do
+  while ! wget -q --progress=dot:mega --timeout=30 --tries=10 --retry-connrefused "https://github.com/Evgenykravchenko/3x-ui-reverse-proxy/raw/refs/heads/main/adh/AdGuardHome.yaml" -O AdGuardHome/AdGuardHome.yaml; do
     warning " $(text 38) "
     sleep 3
   done
@@ -1756,7 +1756,7 @@ EOF
 install_panel() {
   info " $(text 46) "
 
-  while ! wget -q --progress=dot:mega --timeout=30 --tries=10 --retry-connrefused https://raw.githubusercontent.com/cortez24rus/xui-reverse-proxy/refs/heads/main/database/x-ui.db; do
+  while ! wget -q --progress=dot:mega --timeout=30 --tries=10 --retry-connrefused https://github.com/Evgenykravchenko/3x-ui-reverse-proxy/refs/heads/main/database/x-ui.db; do
       warning " $(text 38) "
       sleep 3
   done
@@ -1874,7 +1874,7 @@ EOF
 ###################################
 install_bot() {
   info " $(text 57) "
-  bash <(curl -Ls https://github.com/cortez24rus/xui-reverse-proxy/raw/refs/heads/main/reverse_proxy_bot.sh) "$BOT_TOKEN" "$ADMIN_ID" "$DOMAIN"
+  bash <(curl -Ls https://github.com/Evgenykravchenko/3x-ui-reverse-proxy/raw/refs/heads/main/reverse_proxy_bot.sh) "$BOT_TOKEN" "$ADMIN_ID" "$DOMAIN"
   tilda "$(text 10)"
 }
 
